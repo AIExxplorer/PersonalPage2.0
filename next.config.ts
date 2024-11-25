2cccf8c49d9ca94a5d60ba60e8a5ecd6a74e0c74
Wagner Rafael | AIExxplorer <202305188631@alunos.estacio.br>
2024-11-25 05:19:37 -0300

    fix: bugs

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  swcMinify: true, // Habilitar SWC para minificação mais rápida
   images: {
     domains: [], // Adicione domínios permitidos para carregar imagens externas
     formats: ["image/avif", "image/webp"], // Suporte a formatos modernos de imagens
